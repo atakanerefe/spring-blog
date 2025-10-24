139216528189f505ba554602b7f08ca42c529a97
atakanerefe <122394000+atakanerefe@users.noreply.github.com>
2025-10-24 06:58:00 -0400

    Create CategoryRepository.java

---
+++ src/main/java/com/erefe/blog/repository/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.erefe.blog.repository;
+
+import com.erefe.blog.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category,Long> {
+
+}
